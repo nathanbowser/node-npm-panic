3bfac0e4efd202c0a3c833673dcf0adcb628591b
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2013-05-06 20:15:13 -0400

    Adds an endpoint for fetching the downloads data of a module.

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
 var request = require('request')
   , transfuse = require('transfuse')
-  , url = 'http://isaacs.couchone.com/registry/_design/app/_view/browseUpdated?descending=true&limit=10&skip=0&group_level=3'
+  , recentUrl = 'http://isaacs.couchone.com/registry/_design/app/_view/browseUpdated?descending=true&limit=10&skip=0&group_level=3'
   , http = require('http')
   , Json2Csv = require('json2csv-stream')
+  , moment = require('moment')
+  , url = require('url')
 
-function mapper (doc, map) {
-  map({
-    title: doc[1],
-    updated: doc[0],
-    description: doc[2]
-  })
+function recents () {
+  return request(recentUrl).pipe(transfuse(['rows', /./, 'key'], function (doc, map) {
+    map({
+      title: doc[1],
+      updated: moment(doc[0]).fromNow(),
+      description: doc[2]
+    })
+  }))
 }
 
-function transfused () {
-  return request(url).pipe(transfuse(['rows', /./, 'key'], mapper))
+function downloads (module) {
+  return request({
+    url: 'http://isaacs.iriscouch.com/downloads/_design/app/_view/pkg?',
+    qs: {
+      group_level: 2,
+      start_key: '["' + module + '"]', // wtf
+      end_key: '["' + module + '", {}]',
+    }
+  }).pipe(transfuse(['rows', /./], function (doc, map) {
+    map({
+      request: doc.key[1],
+      downloads: doc.value
+    })
+  }))
 }
 
 http.createServer(function (req, res) {
-  if (req.url === '/recent.json') {
+  var _url = url.parse(req.url, true)
+    , path = _url.pathname
+  if (path === '/recent.json') {
     res.writeHead(200, {'Content-Type': 'application/json'})
-    transfused().pipe(res)
-  } else if (req.url === '/recent.csv') {
+    recents().pipe(res)
+  } else if (path === '/recent.csv') {
     res.writeHead(200, {'Content-Type': 'text/csv'})
     var parser = new Json2Csv
-    transfused().pipe(parser).pipe(res)
+    recents().pipe(parser).pipe(res)
+  } else if (path === '/downloads.csv') {
+    res.writeHead(200, {'Content-Type': 'text/csv'})
+    var parser = new Json2Csv
+    downloads(_url.query.module).pipe(parser).pipe(res)
   } else {
     res.writeHead(400)
     res.end()
   }
-}).listen(1337, '127.0.0.1')
+}).listen(1337, 'dash-dev.com')
+
--- readme.md
+++ readme.md
@@ -1,10 +1,12 @@
 `Node npm panic`
 
-A simple web app that provides endpoints for Panic's [Status Board](http://panic.com/statusboard/), returning json and csv graphs for recently updated [npmjs.org/](NPM Modules)
+A simple web app that provides endpoints for Panic's [Status Board](http://panic.com/statusboard/), returning json and csv dat a for [npmjs.org/](NPM Modules)
 
 ```
   node index.js
 ```
+
+Find the last 10 recently updated npm modules, returned as json (useful for panic tables)
 
 ```
   curl localhost:1337/recent.json
@@ -12,6 +14,14 @@ A simple web app that provides endpoints for Panic's [Status Board](http://panic
 
 or
 
+Find the last 10 recently updated npm modules, as csv (useful for panic tables)
+
 ```
   curl localhost:1337/recent.csv
+```
+
+The numbers of downloads a module has for the last 10 days (used for panic charts)
+
+```
+  curl localhost:1337/downloads.csv?module=request
 ```
